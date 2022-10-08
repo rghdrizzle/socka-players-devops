c9122663d4bd210641c554df4279406e718db728
Luqmaan Hakeem <92255903+rghdrizzle@users.noreply.github.com>
2022-10-08 16:32:23 +0530

    Update getstate.sh

--- getstate.sh
+++ getstate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HTTP_RESPONSE=$(curl \
-     --header "Authorization: Bearer "$TFC_TOKEN"" \
+     --header "Authorization: Bearer "$TF_API"" \
      --header "Content-Type: application/vnd.api+json" \
      "https://app.terraform.io/api/v2/workspaces/"$WORKSPACE_ID"/current-state-version" | jq -r '.data | .attributes | ."hosted-state-download-url"')
 
