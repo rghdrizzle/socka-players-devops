4ee17742ba57ced86933763fa46f2fb5af5d4a55
Luqmaan Hakeem <92255903+rghdrizzle@users.noreply.github.com>
2022-10-08 16:47:14 +0530

    Delete getstate.sh

--- getstate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HTTP_RESPONSE=$(curl \
-     --header "Authorization: Bearer "$TF_API"" \
-     --header "Content-Type: application/vnd.api+json" \
-     "https://app.terraform.io/api/v2/workspaces/"$WORKSPACE_ID"/current-state-version" | jq -r '.data | .attributes | ."hosted-state-download-url"')
-
-curl -o state.tfstate $HTTP_RESPONSE
